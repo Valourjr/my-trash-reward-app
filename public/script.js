6c53851de999512bd5528c7f1e5aebb332afddca
hamidnuhu2019@gmail.com <je161892@gmail.com>
2025-05-05 12:03:00 +0100

    updated project files

--- public/script.js
+++ public/script.js
@@ -16,3 +16,22 @@ document.querySelector('.submit-btn').addEventListener('mouseover', function() {
 document.querySelector('.submit-btn').addEventListener('mouseout', function() {
     this.style.transform = 'scale(1)';
 });
+
+
+let startX = 0;
+
+document.querySelector('.neon-collage').addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+});
+
+document.querySelector('.neon-collage').addEventListener('touchend', (e) => {
+  let endX = e.changedTouches[0].clientX;
+  if (Math.abs(endX - startX) > 50) {
+    document.querySelectorAll('.panel').forEach(p => {
+      p.style.animation = 'shake 0.5s';
+      p.addEventListener('animationend', () => {
+        p.style.animation = '';
+      }, { once: true });
+    });
+  }
+});
